a339f33cbe0d288eb5025d9520cf089d72d11df3
andrew-canafe <andrew.g.canafe@gmail.com>
2021-08-04 12:27:54 -0700

    Remove thrown exception declarations

--- TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/service/ContainersService.java
+++ TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/service/ContainersService.java
@@ -1,5 +1,7 @@
 package cogent.tcvm.service;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,11 @@ public class ContainersService {
 	@Autowired
 	private ContainersRepository consRepo;
 	
-	public Containers getMaxCapacity() {
+	public Containers getMaxCapacity() throws EntityNotFoundException {
 		return consRepo.getById(1);
 	}
 	
-	public Containers getAvailable() {
+	public Containers getAvailable() throws EntityNotFoundException {
 		return consRepo.getById(2);
 	}
 	
