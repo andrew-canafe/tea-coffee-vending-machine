fad520dab474cdb5106be4d38e743e0254255f38
andrew-canafe <andrew.g.canafe@gmail.com>
2021-08-05 10:56:45 -0700

    Add ingredient model and repository

---
+++ TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/repository/IngredientRepository.java
@@ -0,0 +1,9 @@
+package cogent.tcvm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cogent.tcvm.model.Ingredient;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+
+}
