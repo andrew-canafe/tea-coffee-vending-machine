7faf1c5a6debfdd9fec6dcfd313cde04e2e3a682
andrew-canafe <andrew.g.canafe@gmail.com>
2021-08-06 09:08:51 -0700

    Polish refill details and refill row

--- TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/wrapper/RefillDetails.java
+++ TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/wrapper/RefillDetails.java
@@ -9,60 +9,47 @@ import java.util.Map;
 import cogent.tcvm.model.Refill;
 
 public class RefillDetails {
-	
+
 	private List<RefillRow> refillRowList;
-	
+
 	public RefillDetails() {
 		super();
 	}
 
 	public RefillDetails(List<Refill> refillList) {
 		super();
-		
+
 		refillRowList = new ArrayList<RefillRow>();
 		Map<String, RefillRow> refillRowMap = new HashMap<String, RefillRow>();
 		LocalDate today = LocalDate.now();
-		
+
 		for (Refill refill : refillList) {
 			String name = refill.getIngredient().getName();
 			int quantity = refill.getQuantity(); // Used for allDays and today
 			float unitQuantity = refill.getUnitQuantity();
 			String unitType = refill.getIngredient().getUnitType();
-			
+
 			if (refillRowMap.containsKey(name)) {
 				RefillRow refillRowOld = refillRowMap.get(name);
-				
+
 				if (refill.getDate().equals(today)) {
-					refillRowMap.put(name, new RefillRow(name,
-							quantity+refillRowOld.getAllDays(),
-							quantity+refillRowOld.getToday(),
-							unitQuantity+refillRowOld.getUnitQuantity(),
-							unitType));
+					refillRowMap.put(name,
+							new RefillRow(name, quantity + refillRowOld.getAllDays(),
+									quantity + refillRowOld.getToday(), unitQuantity + refillRowOld.getUnitQuantity(),
+									unitType));
 				} else {
-					refillRowMap.put(name, new RefillRow(name,
-							quantity+refillRowOld.getAllDays(),
-							refillRowOld.getToday(),
-							unitQuantity+refillRowOld.getUnitQuantity(),
-							unitType));
+					refillRowMap.put(name, new RefillRow(name, quantity + refillRowOld.getAllDays(),
+							refillRowOld.getToday(), unitQuantity + refillRowOld.getUnitQuantity(), unitType));
 				}
 			} else {
 				if (refill.getDate().equals(today)) {
-					refillRowMap.put(name, new RefillRow(
-							name,
-							quantity,
-							quantity,
-							unitQuantity,
-							unitType));
+					refillRowMap.put(name, new RefillRow(name, quantity, quantity, unitQuantity, unitType));
 				} else {
-					refillRowMap.put(name, new RefillRow(name,
-							quantity,
-							0,
-							unitQuantity,
-							unitType));
+					refillRowMap.put(name, new RefillRow(name, quantity, 0, unitQuantity, unitType));
 				}
 			}
 		}
-		
+
 		this.refillRowList = new ArrayList<RefillRow>(refillRowMap.values()); // Convert hash map to list
 	}
 
@@ -78,5 +65,5 @@ public class RefillDetails {
 	public String toString() {
 		return "RefillDetails [refillRowList=" + refillRowList + "]";
 	}
-	
+
 }
