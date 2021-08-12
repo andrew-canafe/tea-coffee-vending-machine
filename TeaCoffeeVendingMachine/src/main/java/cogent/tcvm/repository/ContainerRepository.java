4636db112b8e445072239e71f88c33a76fbab82a
andrew-canafe <andrew.g.canafe@gmail.com>
2021-08-12 09:23:23 -0700

    Add stereotype annotations to all repositories

--- TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/repository/ContainerRepository.java
+++ TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/repository/ContainerRepository.java
@@ -1,10 +1,12 @@
 package cogent.tcvm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cogent.tcvm.model.Container;
 import cogent.tcvm.model.Ingredient;
 
+@Repository
 public interface ContainerRepository extends JpaRepository<Container, Integer> {
 
 	Container findByIngredient(Ingredient ingredient);
--- TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/repository/IngredientRepository.java
+++ TeaCoffeeVendingMachine/src/main/java/cogent/tcvm/repository/IngredientRepository.java
@@ -1,9 +1,11 @@
 package cogent.tcvm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cogent.tcvm.model.Ingredient;
 
+@Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 
 	Ingredient findByName(String name);
